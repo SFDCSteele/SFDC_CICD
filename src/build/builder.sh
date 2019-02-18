ca783a6ec7f6a6c82c9684223a9ba9969cf67393
William Steele <wsteele@salesforce.com>
2019-02-18 10:58:03 -0500

    Update builder.sh

--- src/build/builder.sh
+++ src/build/builder.sh
@@ -4,8 +4,9 @@ which ant
 ls -al /usr/bin/ant
 ls -al /var/lib/jenkins/
 ant -version
-cp ./src/build/ant-salesforce.jar ~/.ant/ant/lib/
-cp ./src/build/ant-contrib-1.0b3.jar ~/.ant/ant/lib/
+mkdir lib
+cp ./src/build/ant-salesforce.jar ./lib/
+cp ./src/build/ant-contrib-1.0b3.jar ./lib/
 env
 echo "builder.sh: branch is $BRANCH_NAME"
 #add a while look here to take a list of environments to loop through
@@ -70,8 +71,8 @@ echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
 if [ "$1" = "FULL" ]
 then
     echo "Deploying the FULL package...."
-    ant -propertyfile src/build/build.properties -buildfile src/build/build.xml  deploy-full
+    ant -lib ./lib -propertyfile src/build/build.properties -buildfile src/build/build.xml  deploy-full
 else
     echo "Deploying a single package...."
-    ant -propertyfile src/build/build.properties -buildfile src/build/build.xml deploy
+    ant -lib ./lib -propertyfile src/build/build.properties -buildfile src/build/build.xml deploy
 fi
